de4a84ae9df5efa269de191c57f80004fdef3f90
Dickerson, Dominick <92421959+Dominickator@users.noreply.github.com>
2023-07-12 17:33:38 -0500

    Update archscript.sh

--- archscript.sh
+++ archscript.sh
@@ -1,4 +1,5 @@
 #Install yay and other drivers
+echo "Installing yay and other drivers..."
 cd
 cd Documents
 sudo pacman -S --needed git base-devel
@@ -26,12 +27,15 @@ sudo make install
 sudo udevadm control --reload-rules && sudo udevadm trigger
 
 #Now install stuff from the AUR
+echo "Installing utilities from the AUR"
 echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" visual-studio-code-bin swift-bin google-chrome 
 
 #Gaming and stuff
+echo "Installing programs for gaming..."
 sudo pacman -S steam lutris discord
 sudo pacman -S gamemode lib32-gamemode
 
 #Install some other stuff
+echo "Installing other utilities..."
 sudo pacman -S neofetch htop
 flatpak install flathub sh.cider.Cider
