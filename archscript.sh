741bdac6cb9f7a34ecbb283e7d562f6e34249acf
Dickerson, Dominick <92421959+Dominickator@users.noreply.github.com>
2024-04-09 13:06:47 -0500

    Update archscript.sh

--- archscript.sh
+++ archscript.sh
@@ -30,11 +30,26 @@ sudo udevadm control --reload-rules && sudo udevadm trigger
 
 #Now install stuff from the AUR
 echo "Installing utilities from the AUR"
-paru -S visual-studio-code-bin ttf-firacode sf-fonts microsoft-edge-dev-bin zoom
+paru -S visual-studio-code-bin ttf-firacode sf-fonts microsoft-edge-dev-bin ttf-cascadia-code-nerd ttf-inter vesktop-bin
+
+echo -n "Install Thorium Browser? (y/n)"
+read -r response
+
+# Set default to Y if no response is given
+response=${response:-Y}
+
+# Check if the response is either Y or y
+if [[ $response =~ ^([Yy]|YES|yes|Yes)$ ]]
+then
+    echo "Installing Thorium"
+    paru -S thorium-browser-bin
+else
+    echo "Not installing Thorium"
+fi
 
 #Gaming and stuff
 echo "Installing programs for gaming..."
-sudo pacman -S steam lutris discord
+sudo pacman -S steam lutris
 sudo pacman -S gamemode lib32-gamemode
 
 #Install some other stuff
@@ -49,10 +64,14 @@ sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/bobafetthotmail/ref
 
 #Copy games over
 echo "Copying steam games over from Windows"
-sudo mount /dev/nvme0n1p3 /mnt
+sudo mount /dev/nvme1n1p3 /mnt
 cd /mnt/Windows/Fonts
 sudo cp -r * /usr/share/fonts
 sudo fc-cache -fv
+
+#Good looking fonts
+sudo ln -sf /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
+echo "FREETYPE_PROPERTIES="cff:no-stem-darkening=0 autofitter:no-stem-darkening=0"" | sudo tee -a /etc/environment > /dev/null
 
 reboot
 
